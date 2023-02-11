815cb105484ebbb9a1857c6ee696fe3bdc09b8ea
sedat-unal1 <sedatunal42@gmail.com>
2023-02-12 01:55:43 +0300

    delete and edit slider

--- yonetim/slider.php
+++ yonetim/slider.php
@@ -141,16 +141,7 @@
                 <!-- ./ Content -->
 
                 <!-- Footer -->
-                <footer class="content-footer">
-                    <div>© 2020 Gogi - <a href="http://laborasyon.com" target="_blank">Laborasyon</a></div>
-                    <div>
-                        <nav class="nav">
-                            <a href="https://themeforest.net/licenses/standard" class="nav-link">Licenses</a>
-                            <a href="#" class="nav-link">Change Log</a>
-                            <a href="#" class="nav-link">Get Help</a>
-                        </nav>
-                    </div>
-                </footer>
+                <?php include('inc/footer.php'); ?>
                 <!-- ./ Footer -->
             </div>
             <!-- ./ Content body -->
